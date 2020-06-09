ded31c05dc4b6d6062d6a69cda035fc8e0924120
Jalaj Agarwal <Jalaj@Users-MacBook-Pro.local>
2020-06-09 14:17:17 +1000

    Updates

---
+++ precache-manifest.9989cfce4e3c50a169995683f4bdfb03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4962cf4deaef4cf2518bb18adbe89953",
+    "url": "/Monsters_Rolodex/index.html"
+  },
+  {
+    "revision": "ce7617acf8395143e8f6",
+    "url": "/Monsters_Rolodex/static/css/main.4c95921c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters_Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters_Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7617acf8395143e8f6",
+    "url": "/Monsters_Rolodex/static/js/main.21d98ea0.chunk.js"
+  },
+  {
+    "revision": "5f5e1aef01a9836d2b1c",
+    "url": "/Monsters_Rolodex/static/js/runtime-main.79094c6a.js"
+  }
+]);
